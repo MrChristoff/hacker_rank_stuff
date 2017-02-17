ac640c587f5e85b044334ca8b1a8e82393cc5579
Chris Lawrence <chris.lawrence@gmx.com>
2017-02-17 22:16:20 +0000

    tests written for all conversions

--- simple_converter/spec/converter_spec.rb
+++ simple_converter/spec/converter_spec.rb
@@ -10,12 +10,28 @@ describe "Simple converter" do
   end
   describe "Conversions" do
     let (:converter) {SimpleConverter.new}
+    celsius = 10
+    kelvin = 283.15
+    fahrenheit = 50
+    p celsius
 
     it "converts 'celsius' to 'kelvin'" do
-      expect(converter.convert_temp(10, input_scale: 'celsius', output_scale: 'kelvin')).to eq(283.15)
+      expect(converter.convert_temp(celsius, input_scale: 'celsius', output_scale: 'kelvin')).to eq(kelvin)
+    end
+    it "converts 'kelvin' to 'celsius'" do
+      expect(converter.convert_temp(kelvin, input_scale: 'kelvin', output_scale: 'celsius')).to eq(celsius)
     end
     it "converts 'celsius' to 'fahrenheit'" do
-      expect(converter.convert_temp(10, input_scale: 'celsius', output_scale: 'fahrenheit')).to eq(50)
+      expect(converter.convert_temp(celsius, input_scale: 'celsius', output_scale: 'fahrenheit')).to eq(fahrenheit)
+    end
+    it "converts 'fahrenheit' to 'celsius'" do
+      expect(converter.convert_temp(fahrenheit, input_scale: 'fahrenheit', output_scale: 'celsius')).to eq(celsius)
+    end
+    it "converts 'fahrenheit' to 'kelvin'" do
+      expect(converter.convert_temp(fahrenheit, input_scale: 'fahrenheit', output_scale: 'kelvin')).to eq(kelvin)
+    end
+    it "converts 'kelvin' to 'fahrenheit'" do
+      expect(converter.convert_temp(kelvin, input_scale: 'kelvin', output_scale: 'fahrenheit')).to eq(fahrenheit)
     end
   end
 end
