7ec5578830869829b0e6cc226092536e0abd83e6
Chris Lawrence <chris.lawrence@gmx.com>
2017-03-08 11:10:03 +0000

    Apple and Orange - refactored

--- CSHARP/AppleAndOranges.cs
+++ CSHARP/AppleAndOranges.cs
@@ -7,48 +7,39 @@ class Solution {
     static void Main(String[] args)
     {
         string[] tokens_s = Console.ReadLine().Split(' ');
-        int s = Convert.ToInt32(tokens_s[0]);
-        int t = Convert.ToInt32(tokens_s[1]);
+        int houseStart = Convert.ToInt32(tokens_s[0]);
+        int houseEnd = Convert.ToInt32(tokens_s[1]);
         string[] tokens_a = Console.ReadLine().Split(' ');
-        int a = Convert.ToInt32(tokens_a[0]);
-        int b = Convert.ToInt32(tokens_a[1]);
+        int appleTreePos = Convert.ToInt32(tokens_a[0]);
+        int orangeTreePos = Convert.ToInt32(tokens_a[1]);
         string[] tokens_m = Console.ReadLine().Split(' ');
-        int m = Convert.ToInt32(tokens_m[0]);
-        int n = Convert.ToInt32(tokens_m[1]);
+        int appleNum = Convert.ToInt32(tokens_m[0]);
+        int orangeNum = Convert.ToInt32(tokens_m[1]);
         string[] apple_temp = Console.ReadLine().Split(' ');
         int[] apple = Array.ConvertAll(apple_temp,Int32.Parse);
         string[] orange_temp = Console.ReadLine().Split(' ');
         int[] orange = Array.ConvertAll(orange_temp,Int32.Parse);
         int appleCount = 0;
         int orangeCount = 0;
-        List<int> houseRange = new List<int>();
 
-        List<int> myList = new List<int>();
-
-        for (int i = s; i <= t; i++)
+        foreach(int fruitPosition in apple)
         {
-          houseRange.Add(i);
+          if (appleTreePos + fruitPosition >= houseStart && appleTreePos + fruitPosition <= houseEnd )
+          {
+            appleCount++ ;
+            // Console.WriteLine(appleCount);
+          }
         }
 
-        foreach(int dVal in apple)
+        foreach(int fruitPosition in orange)
         {
-            int absVal = dVal + a;
-            if (houseRange.Contains(absVal))
-            {
-                appleCount++ ;
-            }
-
+          if (orangeTreePos + fruitPosition >= houseStart && orangeTreePos + fruitPosition <= houseEnd )
+          {
+            orangeCount++ ;
+            // Console.WriteLine(appleCount);
+          }
         }
 
-        foreach(int dVal in orange)
-        {
-            int absVal = dVal + b;
-            if (houseRange.Contains(absVal))
-            {
-                orangeCount++ ;
-            }
-
-        }
 
         Console.WriteLine(appleCount);
         Console.WriteLine(orangeCount);
